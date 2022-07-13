10d3be59ebc29d60b2549dd46135ea13c09ef3e5
Leos <highwind26@gmail.com>
2022-07-13 18:59:34 +0900

    Add Page

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About () {
+    return (
+        <div>
+            <h1>
+                About Us Leo NextJS 
+            </h1>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <h2>
+          Welcome to <a href="/about">About Page!</a>
+        </h2>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
